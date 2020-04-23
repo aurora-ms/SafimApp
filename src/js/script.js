20b9537022b440385346ea4c90a5544419065e6b
aurora <aurorams94@gmail.com>
2020-04-23 12:37:19 +0200

    Inicio de sesion con Google

--- src/js/script.js
+++ src/js/script.js
@@ -224,3 +224,50 @@ loginButtonSection.addEventListener('click', () => {
         });
 
 })
+
+
+// Inicio de sesión con Google
+
+
+var googleLogin = document.getElementById('googleLogin')
+
+googleLogin.addEventListener('click', () => {
+
+    var selectedSection = document.getElementById('loginSection');
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+
+        // The signed-in user info.
+        var user = result.user;
+
+        var uidUserGoogle = firebase.database().ref('usersSafim/' + user.uid);
+
+        uidUserGoogle.set({
+            User: user.displayName,
+            Email: user.email,
+        });
+
+        initAnimation(user.displayName, selectedSection, " bienvenido")
+
+
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        console.log(errorCode)
+
+        var errorMessage = error.message;
+        console.log(errorMessage)
+
+        // The email of the user's account used.
+        var email = error.email;
+        console.log(email)
+
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        console.log(credential)
+
+        // ...
+    });
+})
+
